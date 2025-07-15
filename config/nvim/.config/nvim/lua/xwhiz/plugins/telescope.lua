633040d336dfdddc2895f2d86590e4176b4d96d0
Hamza <muhammadhamzam1486@gmail.com>
2025-07-15 19:42:46 +0500

    feat: change places for C-p, and <leader>pf

--- config/nvim/.config/nvim/lua/xwhiz/plugins/telescope.lua
+++ config/nvim/.config/nvim/lua/xwhiz/plugins/telescope.lua
@@ -29,10 +29,15 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
+
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [F]iles' })
+    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[P]roject git files' })
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string { search = vim.fn.input 'Grep > ' }
+    end, { desc = '[S]earch the whole [P]roject by string' })
+
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject Git [F]iles' })
-    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
