c133263c956c442cd0382251a30b571ce81dfbc6
Hamza <muhammadhamzam1486@gmail.com>
2025-07-15 19:43:35 +0500

    feat: configure treesitter

--- nvim-created/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-created/.config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "ruby",
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "typescript",
-          "json",
-          "lua",
-        },
-        highlight = { enable = true },
-        indent = { enable = false },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				sync_install = false,
+				ignore_install = {},
+				auto_install = true,
+				ensure_installed = {
+					"c",
+					"bash",
+					"ruby",
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"typescript",
+					"json",
+					"help",
+					"lua",
+					"go",
+				},
+				highlight = { enable = true },
+				indent = { enable = false },
+			})
+		end,
+	},
 }
