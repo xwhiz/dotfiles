d37bcec711559cda34b084fa05551b0797f15b3b
Hamza <muhammadhamzam1486@gmail.com>
2025-07-16 22:34:13 +0500

    feat: improve things a bit

--- config/nvim/.config/nvim/lua/xwhiz/plugins/nvim-lspconfig.lua
+++ config/nvim/.config/nvim/lua/xwhiz/plugins/nvim-lspconfig.lua
@@ -186,9 +186,11 @@ return {
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       -- But for many setups, the LSP (`ts_ls`) will work just fine
-      ts_ls = {},
+      -- ts_ls = {},
       html = {},
       bashls = {},
+      markdown_oxide = {},
+
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
